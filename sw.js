cab2060b6433cd6f3ca890cdf6e8f4ecc805265a
Baruch Elias <eyesweyedopen@gmail.com>
2018-11-12 16:13:13 -0500

    Add sw.js code back with altered cacheArray

--- sw.js
+++ sw.js
@@ -10,30 +10,29 @@ self.addEventListener("register", (e) => {
 self.addEventListener('install', (e) => {
     staticCacheName = `SWv1`;
     const cacheArray = [
-        "/",
-        "/data/restaurants.json",
-        "/index.html",
-        "/restaurant.html",
-        "/js/main.js",
-        "/js/restaurant_info.js",
-        "/img/1.jpg",
-        "/img/2.jpg",
-        "/img/3.jpg",
-        "/img/4.jpg",
-        "/img/5.jpg",
-        "/img/6.jpg",
-        "/img/7.jpg",
-        "/img/8.jpg",
-        "/img/9.jpg",
-        "/img/10.jpg",
+        "data/restaurants.json",
+        "index.html",
+        "restaurant.html",
+        "js/main.js",
+        "js/restaurant_info.js",
+        "img/1.jpg",
+        "img/2.jpg",
+        "img/3.jpg",
+        "img/4.jpg",
+        "img/5.jpg",
+        "img/6.jpg",
+        "img/7.jpg",
+        "img/8.jpg",
+        "img/9.jpg",
+        "img/10.jpg",
     ]
 
     
     e.waitUntil(
         caches.open(staticCacheName)
-            // .then((newCache) => {
-            //     return newCache.addAll(cacheArray)
-            // })
+            .then((newCache) => {
+                return newCache.addAll(cacheArray)
+            })
     );
 });
 
