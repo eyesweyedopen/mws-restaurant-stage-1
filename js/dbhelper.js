ed6a67cb2116ae8e0a3b3f296cf861d7ba03e10c
Baruch Elias <eyesweyedopen@gmail.com>
2018-11-12 16:06:19 -0500

    Chenge route to ./sw for registry

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -4,7 +4,7 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('./sw.js');
   });
 }
 
