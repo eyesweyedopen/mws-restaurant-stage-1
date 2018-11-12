41667095efd6d70d5a88697e12e1389f78001efd
Baruch Elias <eyesweyedopen@gmail.com>
2018-11-12 15:14:11 -0500

    Add net logging to sw.js

--- sw.js
+++ sw.js
@@ -11,21 +11,21 @@ self.addEventListener('install', (e) => {
     staticCacheName = `SWv1`;
     const cacheArray = [
         "/",
-        "./data/restaurants.json",
-        "index.html",
-        "restaurant.html",
-        "./js/main.js",
-        "./js/restaurant_info.js",
-        "./img/1.jpg",
-        "./img/2.jpg",
-        "./img/3.jpg",
-        "./img/4.jpg",
-        "./img/5.jpg",
-        "./img/6.jpg",
-        "./img/7.jpg",
-        "./img/8.jpg",
-        "./img/9.jpg",
-        "./img/10.jpg",
+        "/data/restaurants.json",
+        "/index.html",
+        "/restaurant.html",
+        "/js/main.js",
+        "/js/restaurant_info.js",
+        "/img/1.jpg",
+        "/img/2.jpg",
+        "/img/3.jpg",
+        "/img/4.jpg",
+        "/img/5.jpg",
+        "/img/6.jpg",
+        "/img/7.jpg",
+        "/img/8.jpg",
+        "/img/9.jpg",
+        "/img/10.jpg",
     ]
     
     e.waitUntil(
@@ -55,6 +55,7 @@ self.addEventListener('fetch', (e) => {
                 if (res) {
                     return res;
                 }
+                console.log(e.request);
                 
                 caches.open('SWv1').then((cache) => {
                     //         cache.add(e.request);
