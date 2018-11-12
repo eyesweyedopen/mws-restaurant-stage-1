1ddae5a9a139bfaee81c53ce9f429edd38d30559
Baruch Elias <eyesweyedopen@gmail.com>
2018-11-12 15:38:40 -0500

    change full path to one resource sw.js

--- sw.js
+++ sw.js
@@ -15,17 +15,17 @@ self.addEventListener('install', (e) => {
         "/index.html",
         "/restaurant.html",
         "/js/main.js",
-        "/js/restaurant_info.js"
-        // "/img/1.jpg",
-        // "/img/2.jpg",
-        // "/img/3.jpg",
-        // "/img/4.jpg",
-        // "/img/5.jpg",
-        // "/img/6.jpg",
-        // "/img/7.jpg",
-        // "/img/8.jpg",
-        // "/img/9.jpg",
-        // "/img/10.jpg",
+        "/js/restaurant_info.js",
+        "/mws-restaurant-stage-1/img/1.jpg",
+        "/img/2.jpg",
+        "/img/3.jpg",
+        "/img/4.jpg",
+        "/img/5.jpg",
+        "/img/6.jpg",
+        "/img/7.jpg",
+        "/img/8.jpg",
+        "/img/9.jpg",
+        "/img/10.jpg",
     ]
 
     
